d6ee08df44e527ee7abfe6b12dee8580ffa1f8ea
Stefani Karp <skarp@princeton.edu>
2015-01-03 14:27:55 -0500

    fixed main.jl

--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,10 @@
 using Medoids
 
 # Test parkJun alg on random instance and ORlib instance
-algs = [Medoids.pam] #[Medoids.parkJun, Medoids.forwardGreedy, Medoids.reverseGreedy, Medoids._reverseGreedyOpt]
-d = 5
-n = 10
-k = 4
+algs = [Medoids.parkJun, Medoids.forwardGreedy, Medoids.reverseGreedy, Medoids._reverseGreedyOpt]
+d = 10
+n = 100
+k = 50
 _, costs = Medoids.randomInstance(d, n)
 Medoids.testInstance(algs, costs, k)
 
